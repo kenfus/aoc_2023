b435a4f31afa470c6b57e7756f109e18cdb04473
Vincenzo <vincenzo.timmel@fhnw.ch>
2023-12-16 00:26:03 +0100

    Added better code.

--- day_12/2.py
+++ day_12/2.py
@@ -1,35 +1,31 @@
-# %%
 with open("input.txt") as f:
     lines = f.readlines()
 
-# %%
+import os
 from functools import cache
+from multiprocessing import Pool
+
+from tqdm import tqdm
 
 
-# %%
+@cache
 def is_valid(s):
-    if "@#" in s or "#@" in s:
-        return False
-    else:
-        return True
+    return not ("@#" in s or "#@" in s)
 
 
-# %%
+@cache
 def place_substring(s, length):
     pos = []
     for i in range(len(s) - length + 1):
+        # Check for # before
+        str_before = s[:i]
+        if "#" in str_before:
+            return tuple(pos)  # No need to check further
         str_overwritten = s[i : i + length]
         if "." in str_overwritten or "@" in str_overwritten:
             continue
-        # Check for # before
-        for j in range(i - 1):
-            if s[j] == "#":
-                return tuple(pos)  # No need to check further
         cur_str = s[:i] + length * "@" + s[i + length :]
-        for j in range(i):
-            if cur_str[j] == "?":
-                cur_str = cur_str[:j] + "." + cur_str[j + 1 :]
-
+        cur_str = cur_str[:i].replace("?", ".") + cur_str[i:]
         if is_valid(cur_str):
             # Replace the chars with '.'
             cur_str = cur_str.replace("@?", "@.").replace("?@", ".@")
@@ -37,11 +33,13 @@ def place_substring(s, length):
     return tuple(pos)
 
 
-# %%
+@cache
 def get_all_pos(s, lengths, total_l):
     if s.count("#") == 0 and s.count("@") == total_l:
         return 1
     if len(lengths) == 0:
+        return 0
+    if s.count("#") > sum(lengths):
         return 0
     else:
         lengths_copy = lengths
@@ -54,44 +52,41 @@ def get_all_pos(s, lengths, total_l):
                 continue
             elif pos.count("#") + pos.count("?") < sum(lengths_copy):
                 continue
+            elif pos.count("#") > sum(lengths_copy):
+                continue
             else:
                 total += get_all_pos(pos, lengths_copy, total_l)
         return total
 
 
-# %%
-examples = []
-for line in lines:
-    example = line.strip().split(" ")
-    example[0] = "?".join([example[0] for _ in range(5)])
-    example[1] = ",".join([example[1] for _ in range(5)])
-    example[1] = [int(x) for x in example[1].split(",")]
-    examples.append(example)
-
-
-# %%
 def process_example(example):
     return get_all_pos(example[0], tuple(example[1]), sum(example[1]))
 
 
-import os
-from multiprocessing import Pool
+def main():
+    examples = []
+    for line in lines:
+        example = line.strip().split(" ")
+        example[0] = "?".join([example[0] for _ in range(5)])
+        example[1] = ",".join([example[1] for _ in range(5)])
+        example[1] = [int(x) for x in example[1].split(",")]
+        examples.append(example)
 
-# %%
-from tqdm import tqdm
+    # Assuming 'examples' is a list of tuples
+    # results = [process_example(example) for example in tqdm(examples)]
 
-# Assuming 'examples' is a list of tuples
-with Pool(os.cpu_count()) as pool:
-    results = list(tqdm(pool.imap(process_example, examples), total=len(examples)))
+    with Pool(os.cpu_count()) as pool:
+        results = list(tqdm(pool.imap(process_example, examples), total=len(examples)))
 
-total = sum(results)
+    total = sum(results)
+
+    len(results)
+
+    print(total)
 
 
-# %%
-len(results)
+if __name__ == "__main__":
+    main()
+    """    import cProfile
 
-# %%
-print(total)
-
-# %%
-7007, 322723, 46660346
+    cProfile.run("main()", "profile_output")"""
